8ff4fa0de9d905b173d839dbf3011cbdf2cae0af
statistikvolker <holzendorf@gmx.eu>
2022-11-06 20:51:50 +0100

    update

--- R-Codes/000_packages_variables.r
+++ R-Codes/000_packages_variables.r
@@ -1,37 +1,26 @@
+
 # 000_packages_variables.r
 
 
 # Pakete laden ------------------------------------------------------------
 
-library(shiny)
-library(shinyjs)
-library(shinybusy)
-library(tidyverse)
-library(readxl)
-library(readtext)
-library(knitr)
+source("R-Codes/001_packages.r")
+#verzeichnis <- "testdaten"  # Test lokal
+# # Benutzerdefinierte Funktionen -----------------------------------------
 
 
 mkr.formatnumeric <- function(val,digits=3){
   fval<-rep(NA,length(val))
-  for(i in (1:length(val))){
+  for(i in (seq_along(val))){
     fval[i]<-formatC(val[i],digits=digits,format="f")
   }
   return(fval)
 }
 
 
-# Glaobale Variablen definieren -----------------------------------------
+get_lastmeld <- function() {
+  result <- read_csv(lastmeldfile, col_types = coltslast)
+  return(result)
+}
 
-
-# Sort the input dataframe into random order (and add an "answered" flag)
-vocdf <- read_excel("EnglischVokabelnacess1.xlsx") %>% 
-  mutate(Random=runif(nrow(.)),
-         CorrectlyAnswered=FALSE
-         ) %>% 
-      arrange(Random)
-names(vocdf)
-   
-# Take a copy for delayed updates
-analysed <- vocdf
 
